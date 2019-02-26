e81443afc38b9781b222451d19e86f2d62a8e28f
willyci <willyci@gmail.com>
2019-02-26 12:45:50 -0500

    fix table bug, clean up js code

--- js/main.js
+++ js/main.js
@@ -33,16 +33,13 @@ function mouse_over_event(){
 
 }
 
-
+// all peas animations 
 function pea_animate_1(){
 	
 	reset_all();
-	
-	var x = 0; 
-	var y = 0;
-	
-	x = $('.pea_1 > img')[0].getBoundingClientRect().top-8+$(window).scrollTop();
-	y = $('.pea_1 > img')[0].getBoundingClientRect().left-1;	
+
+	var x = $('.pea_1 > img')[0].getBoundingClientRect().top-8+$(window).scrollTop();
+	var y = $('.pea_1 > img')[0].getBoundingClientRect().left-1;	
 	$('#ypea1').animate({top:x+'px', left:y+'px'},{ duration: 1000, queue: false ,   complete:function () { $('.pea_1 > img').css('visibility','visible'); }});
 	$('#ypea_svg1').animate({height:'60px', width:'60px'},{ duration: 1000, queue: false , complete:function(){ $('#ypea_svg1').fadeTo({ duration: 500 },0,function(){});  }});
 	$('#gpea1').animate({top:(x-50)+'px', left:y+'px'},{ duration: 1000, queue: false },function(){});
@@ -51,14 +48,9 @@ function pea_animate_1(){
 }
 
 function pea_animate_2(){
-	
-	//reset_all();
-	
-	var x = 0; 
-	var y = 0;
-	
-	x = $('.pea_2 > img')[0].getBoundingClientRect().top-8+$(window).scrollTop();
-	y = $('.pea_2 > img')[0].getBoundingClientRect().left-1;	
+
+	var x = $('.pea_2 > img')[0].getBoundingClientRect().top-8+$(window).scrollTop();
+	var y = $('.pea_2 > img')[0].getBoundingClientRect().left-1;	
 	$('#ypea2').animate({top:x+'px', left:y+'px'},{ duration: 1000, queue: false ,   complete:function () { $('.pea_2 > img').css('visibility','visible'); }});
 	$('#ypea_svg2').animate({height:'60px', width:'60px'},{ duration: 1000, queue: false , complete:function(){ $('#ypea_svg2').fadeTo({ duration: 500 },0,function(){});  }});
 	$('#gpea2').animate({top:(x-50)+'px', left:y+'px'},{ duration: 1000, queue: false },function(){});
@@ -69,13 +61,8 @@ function pea_animate_2(){
 
 function pea_animate_3(){
 	
-
-	
-	var x = 0; 
-	var y = 0;
-	
-	x = $('.pea_3 > img')[0].getBoundingClientRect().top-8+$(window).scrollTop();
-	y = $('.pea_3 > img')[0].getBoundingClientRect().left-1;	
+	var x = $('.pea_3 > img')[0].getBoundingClientRect().top-8+$(window).scrollTop();
+	var y = $('.pea_3 > img')[0].getBoundingClientRect().left-1;	
 	$('#ypea3').animate({top:x+'px', left:y+'px'},{ duration: 1000, queue: false ,   complete:function () { $('.pea_3 > img').css('visibility','visible'); }});
 	$('#ypea_svg3').animate({height:'60px', width:'60px'},{ duration: 1000, queue: false , complete:function(){ $('#ypea_svg3').fadeTo({ duration: 500 },0,function(){});  }});
 	$('#gpea3').animate({top:(x-50)+'px', left:y+'px'},{ duration: 1000, queue: false },function(){});
@@ -86,12 +73,8 @@ function pea_animate_3(){
 
 function pea_animate_4(){
 	
-	
-	var x = 0; 
-	var y = 0;
-	
-	x = $('.pea_4 > img')[0].getBoundingClientRect().top-8+$(window).scrollTop();
-	y = $('.pea_4 > img')[0].getBoundingClientRect().left-1;	
+	var x = $('.pea_4 > img')[0].getBoundingClientRect().top-8+$(window).scrollTop();
+	var y = $('.pea_4 > img')[0].getBoundingClientRect().left-1;	
 	$('#ypea4').animate({top:x+'px', left:y+'px'},{ duration: 1000, queue: false ,   complete:function () { $('.pea_4 > img').css('visibility','visible'); }});
 	$('#ypea_svg4').animate({height:'60px', width:'60px'},{ duration: 1000, queue: false , complete:function(){ $('#ypea_svg4').fadeTo({ duration: 500 },0,function(){});  }});
 	$('#gpea4').animate({top:(x-50)+'px', left:y+'px'},{ duration: 1000, queue: false },function(){});
@@ -99,33 +82,21 @@ function pea_animate_4(){
 	
 }
 
+
+// hide all peas, reset all peas
 function reset_all() {
 	
-		// hide all peas, reset all peas
-	$('.pea_1 > img').css('visibility','hidden');
-	$('.pea_2 > img').css('visibility','hidden');
-	$('.pea_3 > img').css('visibility','hidden');
-	$('.pea_4 > img').css('visibility','hidden');
+	// hide all peas, reset all peas
+	$('.pea_1 > img, .pea_2 > img, .pea_3 > img, .pea_4 > img').css('visibility','hidden');
 	
-	$('#ypea_svg1').css('opacity','1').css('width','40px').css('height','40px');
-	$('#ypea_svg2').css('opacity','1').css('width','40px').css('height','40px');
-	$('#ypea_svg3').css('opacity','1').css('width','40px').css('height','40px');
-	$('#ypea_svg4').css('opacity','1').css('width','40px').css('height','40px');
-	                                  
-	$('#gpea_svg1').css('opacity','1').css('width','40px').css('height','40px');
-	$('#gpea_svg2').css('opacity','1').css('width','40px').css('height','40px');
-	$('#gpea_svg3').css('opacity','1').css('width','40px').css('height','40px');
-	$('#gpea_svg4').css('opacity','1').css('width','40px').css('height','40px');
+	$('#ypea_svg1, #ypea_svg2, #ypea_svg3, #ypea_svg4').css('opacity','1').css('width','40px').css('height','40px');
+
+	$('#gpea_svg1, #gpea_svg2, #gpea_svg3, #gpea_svg4').css('opacity','1').css('width','40px').css('height','40px');
 	
-	$('#ypea1').css('top', 'auto').css('left', 'auto');
-	$('#ypea2').css('top', 'auto').css('left', 'auto');
-	$('#ypea3').css('top', 'auto').css('left', 'auto');
-	$('#ypea4').css('top', 'auto').css('left', 'auto');
+	$('#ypea1, #ypea2, #ypea3, #ypea4').css('top', 'auto').css('left', 'auto');
 	
-	$('#gpea1').css('top', 'auto').css('left', 'auto');
-	$('#gpea2').css('top', 'auto').css('left', 'auto');
-	$('#gpea3').css('top', 'auto').css('left', 'auto');
-	$('#gpea4').css('top', 'auto').css('left', 'auto');
+	$('#gpea1, #gpea2, #gpea3, #gpea4').css('top', 'auto').css('left', 'auto');
+
 	
 }
 
@@ -135,9 +106,11 @@ function log(s){window.console && console.log(s);};
 
 
 $(function() {
-    log( "ready go!" );
-
-	pea_animate_1();
+    //log( "ready go!" );
+	
+	// onload reset all, run after 2 seconds
+	reset_all();
+	setTimeout(function(){ pea_animate_1(); }, 2000);
 	mouse_over_event();
 
 
